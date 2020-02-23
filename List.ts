d58a8c77d5773a41ce252c6bbcf1b363cfde297c
voidwaves <incognitoxyx@googlemail.com>
2020-02-23 20:01:20 +0100

    added List.includes function

--- List.ts
+++ List.ts
@@ -5,12 +5,12 @@ export namespace List {
 
     // replace each element of a list
     export function map<Type>([head, ...tail]: Type[], func: (element: Type) => Type, result = []): Type[] {
-        return (head != undefined) ? map(tail, func, [...result, func(head)]) : result
+        return (head !== undefined) ? map(tail, func, [...result, func(head)]) : result
     }
 
     // run a function for each element of a list
     export function foreach<Type>([head, ...tail]: Type[], func) {
-        if(head != undefined) {
+        if(head !== undefined) {
             func(head)
             foreach(tail, func)
         }
@@ -18,22 +18,27 @@ export namespace List {
 
     // return a list only with elements matching the predicate function
     export function filter<Type>([head, ...tail]: Type[], func: (element: Type) => boolean, result = []): Type[] {
-        return (head != undefined) ? filter(tail, func, [...result, ...( func(head) ? [head] : [] )]) : result
+        return (head !== undefined) ? filter(tail, func, [...result, ...(func(head) ? [head] : [])]) : result
+    }
+
+    // return a boolean, depending on an element existing in a list
+    export function includes<Type>([head, ...tail]: Type[], value: Type, status = true): boolean {
+        return ((head !== undefined) && status) ? includes(tail, value, status && (head === value)) : status
     }
 
     // return a sublist of a given list
     export function sublist<Type>([head, ...tail]: Type[], from: number, to: number, index = 0, result = []): Type[] {  
-        return (head != undefined) ? sublist(tail, from, to, index + 1, [...result, ...((index >= from && index <= to) ? [head] : [])]) : result
+        return (head !== undefined) ? sublist(tail, from, to, index + 1, [...result, ...((index >= from && index <= to) ? [head] : [])]) : result
     }
 
     // determine the length of a given list
     export function size<Type>([head, ...tail]: Type[], count = 0): number {
-        return (head != undefined) ? size(tail, count + 1) : count
+        return (head !== undefined) ? size(tail, count + 1) : count
     }
 
     // reverse a given list
     export function reverse<Type>([head, ...tail]: Type[], result = []): Type[] {
-        return (head != undefined) ? reverse(tail, [head, ...result]) : result
+        return (head !== undefined) ? reverse(tail, [head, ...result]) : result
     }
 }
 
